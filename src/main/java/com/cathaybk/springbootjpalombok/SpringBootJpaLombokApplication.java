8c7423b0c65b64fb730321fe1e0cbed9c4c64303
Roger <sobadrush@icloud.com>
2022-07-26 14:35:27 +0800

    init commit

---
+++ src/main/java/com/cathaybk/springbootjpalombok/SpringBootJpaLombokApplication.java
@@ -0,0 +1,13 @@
+package com.cathaybk.springbootjpalombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJpaLombokApplication.class, args);
+    }
+
+}
