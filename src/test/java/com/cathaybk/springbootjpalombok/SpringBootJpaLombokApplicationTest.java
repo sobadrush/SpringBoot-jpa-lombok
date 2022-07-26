c9260d6d066364b7fbfb6801dc96b9cf0d4b99a1
Roger <sobadrush@icloud.com>
2022-07-26 15:00:21 +0800

    建立SpringBoot單元測試物件

---
+++ src/test/java/com/cathaybk/springbootjpalombok/SpringBootJpaLombokApplicationTest.java
@@ -0,0 +1,14 @@
+package com.cathaybk.springbootjpalombok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootJpaLombokApplicationTest {
+
+    @Test
+    public void contextLoads() {
+        System.err.println(" === SpringBoot Testing: contextLoads === ");
+    }
+
+}
